8e9e08931f19638f52145291b7393327f0eada04
Vic <vic.trouble@gmail.com>
2021-01-08 21:43:19 +0300

    Init

---
+++ Assets/Camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Vector2 offset;
+
+    // Use this for initialization
+    void Start ()
+    {
+        offset = new Vector2(0, 0);
+	}
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform player = GameObject.Find("Shooter").transform;
+
+        int lerpFactor = 40;
+        float x = Mathf.Lerp(transform.position.x, player.position.x + offset.x, Time.deltaTime * lerpFactor);
+        float y = Mathf.Lerp(transform.position.y, player.position.y + offset.y, Time.deltaTime * lerpFactor);
+
+        transform.position = new Vector3(x, y, transform.position.z); // Camera follows the player with specified offset position
+    }
+}
---
+++ Assets/Units/Shooter/ShooterController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterController : MonoBehaviour
+{
+    // primary animation
+    private Animator animator;
+
+    private float SPEED = 0.25f;
+
+    private bool isRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per tick
+    void FixedUpdate()
+    {
+        if (Input.GetButton("Fire1")) {
+            animator.Play("Shoot");
+            return;
+        }
+
+        // else move
+        float move = Input.GetAxis("Horizontal") * SPEED;
+
+        transform.position = new Vector3(transform.position.x + move, transform.position.y, transform.position.z);
+
+        if (Mathf.Abs(move) > 0.01)
+            animator.Play("Walk");
+        else
+            animator.Play("Idle");
+
+        //  flip it
+        if (isRight && move < 0) {
+            isRight = false;
+            transform.localScale = new Vector3(-1, 1, 1);
+        } else if (!isRight && move > 0) {
+            isRight = true;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+}
