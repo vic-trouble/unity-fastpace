ad5948fcc7176badd976d0f6ce39e9fd83fc8ba7
Vic <vic.trouble@gmail.com>
2021-01-08 23:13:59 +0300

    Walk animation

--- Assets/Units/Shooter/ShooterController.cs
+++ Assets/Units/Shooter/ShooterController.cs
@@ -8,18 +8,53 @@ public class ShooterController : MonoBehaviour
     private Animator animator;
 
     // movement speed
-    public float SPEED = 0.25f;
+    public float SPEED = 1f;
 
     // aim cursor
     public Texture2D aimCursor;
 
     //private bool isRight = true;
 
+    private string currentAnimation;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         Cursor.SetCursor(aimCursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    private string GetAnimPrefix(float direction)
+    {
+        if (direction < 45 || direction >= 360 - 45) {   // right
+            return "";  // used to be 'Side' in sprites
+        }
+        else if (direction >= 180 - 45 && direction < 180 + 45) { // left
+            return "";
+        }
+        else if (direction >= 45 && direction < 90 + 45) {  // up
+            return "Back-";
+        }
+        else {  // down
+            return "Front-";
+        }
+    }
+
+    private int GetFlipX(float direction)
+    {
+        if (direction >= 180 - 45 && direction < 180 + 45) // left
+            return -1;
+
+        return 1;
+    }
+
+    private void PlayAnimatinon(string animation)
+    {
+        if (currentAnimation == animation)
+            return;
+
+        animator.Play(animation);
+        currentAnimation = animation;
     }
 
     // Update is called once per tick
@@ -38,28 +73,22 @@ public class ShooterController : MonoBehaviour
             direction += 360;
         Debug.Log("angle=" + direction);
 
-        if (direction < 45 || direction >= 360 - 45) {   // right
-            animator.Play("Idle");
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else if (direction >= 180 - 45 && direction < 180 + 45) { // left
-            animator.Play("Idle");
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-        else if (direction >= 45 && direction < 90 + 45) {  // up
-            animator.Play("Back-Idle");
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else {  // down
-            animator.Play("Front-Idle");
-            transform.localScale = new Vector3(1, 1, 1);
-        }
+        string animation = "Idle";
 
         // move
-        float moveX = Input.GetAxis("Horizontal") * SPEED;
-        float moveY = Input.GetAxis("Vertical") * SPEED;
-
+        float moveX = Input.GetAxis("Horizontal") * SPEED * Time.fixedDeltaTime;
+        float moveY = Input.GetAxis("Vertical") * SPEED * Time.fixedDeltaTime;
         transform.position = new Vector3(transform.position.x + moveX, transform.position.y + moveY, transform.position.z);
+
+        if (Mathf.Abs(moveX) > 0 || Mathf.Abs(moveY) > 0)
+            animation = "Walk";
+
+        // flip if necessary
+        transform.localScale = new Vector3(GetFlipX(direction), 1, 1);
+
+        // animate
+        PlayAnimatinon(GetAnimPrefix(direction) + animation);
+
 
         //if (Mathf.Abs(moveX) > 0.01)
         //    animator.Play("Walk");
