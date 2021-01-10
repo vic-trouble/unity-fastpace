921e3bdef60362234926619a6c95f7e95d689f35
Vic <vic.trouble@gmail.com>
2021-01-10 12:33:07 +0300

    Proper particles sorting layer

---
+++ Assets/Effects/ParticleSortingLayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSortingLayer : MonoBehaviour
+{
+    public int sortingOrder;
+    public string sortingLayerName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = sortingLayerName;
+        GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingOrder = sortingOrder;
+    }
+}
