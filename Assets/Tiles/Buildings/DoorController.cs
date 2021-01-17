b22f8d6943562c4a48503a8da3a8cffbb90a369d
Vic <vic.trouble@gmail.com>
2021-01-17 23:40:57 +0300

    We've got doors

---
+++ Assets/Tiles/Buildings/DoorController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private enum State
+    {
+        Close,
+        Opening,
+        Open
+    }
+
+    private State state = State.Close;
+
+    private bool open = false;
+
+    private float disableColliderTime = 0;
+
+    void FixedUpdate()
+    {
+        if (disableColliderTime != 0 && Time.fixedTime > disableColliderTime) {
+            GetComponent<Collider2D>().enabled = false;
+            disableColliderTime = 0;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<UnitController>()) {
+            if (!open) {
+                open = false;
+                GetComponent<Animator>().SetBool("Open", true);
+                disableColliderTime = Time.fixedTime + 0.2f;
+            }
+        }
+    }
+}
