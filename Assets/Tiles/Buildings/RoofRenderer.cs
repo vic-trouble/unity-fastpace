76e8694c62fdef06afe074f081f5cfff37d53db3
Vic <vic.trouble@gmail.com>
2021-01-17 17:57:49 +0300

    Buildings have floor & roof

---
+++ Assets/Tiles/Buildings/RoofRenderer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class RoofRenderer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // remove transparency
+        GetComponent<Tilemap>().color = Color.white;
+    }
+}
