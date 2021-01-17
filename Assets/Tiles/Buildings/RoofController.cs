7be782c2848f717d225d8ba122c161acbacdea81
Vic <vic.trouble@gmail.com>
2021-01-17 20:08:01 +0300

    Added portals into indoor

--- Assets/Tiles/Buildings/RoofController.cs
+++ Assets/Tiles/Buildings/RoofController.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public class RoofRenderer : MonoBehaviour
+public class RoofController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         // remove transparency
+        Show();
+    }
+
+    public void Show()
+    {
         GetComponent<Tilemap>().color = Color.white;
+    }
+
+    public void Hide()
+    {
+        GetComponent<Tilemap>().color = new Color(0, 0, 0, 0);
     }
 }
