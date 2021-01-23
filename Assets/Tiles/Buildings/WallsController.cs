8fe57e049d51ecb9cf627e7d9275ed460c43a4eb
Vic <vic.trouble@gmail.com>
2021-01-23 10:20:54 +0300

    Destructible walls redone

--- Assets/Tiles/Buildings/WallsController.cs
+++ Assets/Tiles/Buildings/WallsController.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public struct DestructableTile
+[System.Serializable]
+public struct DestructibleTile
 {
-    public Sprite normal;
-    public Sprite destructed;
+    public Tile tile;
+    public float damageLevel;
+}
+
+[System.Serializable]
+public struct DestructibleWall
+{
+    public List<DestructibleTile> tiles;
 }
 
 public class WallsController : MonoBehaviour
 {
-    //[SerializeField]
-    public List<Tile> destruction;
+    public List<DestructibleWall> destructibleWalls;
 
     private Dictionary<Vector3Int, float> tileDamage = new Dictionary<Vector3Int, float>();
 
@@ -20,16 +26,33 @@ public class WallsController : MonoBehaviour
     {
         var tilemap = GetComponent<Tilemap>();
         Vector3Int tilePosition = tilemap.WorldToCell(position);
+        TileBase curTile = tilemap.GetTile(tilePosition);
+        if (!curTile)
+            return;
+
         if (tileDamage.ContainsKey(tilePosition))
             tileDamage[tilePosition] += damage;
         else
             tileDamage[tilePosition] = damage;
-        if (tileDamage[tilePosition] > 20) {
-            if (tilemap.GetTile(tilePosition) == destruction[0]) {
-                tilemap.SetTile(tilePosition, destruction[1]);
+        float curDamage = tileDamage[tilePosition];
+
+        foreach (var destructibleWall in destructibleWalls) {
+            TileBase newTile = null;
+            foreach (var destructibleTile in destructibleWall.tiles) {
+                if ((SameTile(curTile, destructibleTile.tile) || newTile) && curDamage >= destructibleTile.damageLevel) {
+                    newTile = destructibleTile.tile;
+                }
+            }
+            if (newTile && !SameTile(newTile, curTile)) {
+                tilemap.SetTile(tilePosition, newTile);
                 var effectsController = GameObject.Find("+Effects").GetComponent<EffectsController>();
                 effectsController.RemoveBulletHoles((Vector2Int)tilePosition);
             }
         }
+    }
+
+    private bool SameTile(TileBase tileA, TileBase tileB)
+    {
+        return tileA.name == tileB.name;
     }
 }
